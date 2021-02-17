07c45bf8c9fc7ecc88b865e9814b27827a076b39
Oikos.cash <albert@oikos.cash>
2021-02-17 18:19:19 -0400

    reduce feeLimit to 100 TRX

--- tools/abitojs.js
+++ tools/abitojs.js
@@ -376,7 +376,7 @@ const generateFunctionStr = (abiFn, source) => {
       txParams = txParams || {};
       txParams = {
         // fee limit in SUN
-        feeLimit: 1000000000,
+        feeLimit: 100000000,
         ...txParams
       };
       const txHash = await this.contract.${abiFn.name}(${methodParamsStr}).send(txParams);
